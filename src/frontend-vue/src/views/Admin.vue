17620cdba964efebf4290b5ac6b1cc99308fbe45
Savo Shen <savo_shen@qq.com>
2025-03-11 13:42:19 +0800

    优化管理员入口

--- src/frontend-vue/src/views/Admin.vue
+++ src/frontend-vue/src/views/Admin.vue
@@ -1,84 +1,141 @@
 <script setup>
 
-import {ref} from 'vue'
+import { ref, shallowRef } from 'vue'
+import { defineAsyncComponent } from 'vue';
 
-import { get_students_list_request} from '../api/api';
+import router from '../router';
 
-const studentList = ref()
-// const patientList = ref()
+import {
+    is_login_request,
+} from '../api/api';
 
-async function get_students_list() {
-    studentList.value = await get_students_list_request()
+import Header         from './components/header/Header.vue';
+import SideBar        from './components/SideBar.vue';  
+import Footer         from './components/footer/Footer.vue';
+
+const pageList = shallowRef([
+    {
+        name: '首页',
+        component: defineAsyncComponent(() => import('./pages/Admin.vue'))
+    },
+    {
+        name: '在线HR',
+        component: defineAsyncComponent(() => import('./pages/OnlineHR.vue'))
+    },
+    {
+        name: '测试模块',
+        component: defineAsyncComponent(() => import('./pages/TestModule.vue'))
+    },
+    {
+        name: '题库管理',
+        component: defineAsyncComponent(() => import('./pages/Question.vue')) 
+    },
+    {
+        name: '约面管理',
+        component: defineAsyncComponent(() => import('./pages/Interview.vue'))
+    },
+    {
+        name: '报酬管理',
+        component: defineAsyncComponent(() => import('./pages/Salary.vue'))
+    }
+])
+
+const user                 = ref("")
+
+const selectedName = ref("报酬管理")
+
+function refresh() {
+    window.location.reload()
 }
 
-// async function get_all_patient() {
-//     patientList.value = await get_all_patient_request()
-// }
-
+async function is_login() {
+    var value = await is_login_request()
+    if (value.status == true) {
+        user.value = value.data
+        // if(user.value.is_admin == true){
+        //     var value = await get_students_list_request()
+        //     if(value.status == false) {
+        //         alert(data.message)
+        //     } else {
+        //         studentList.value = value.data
+        //         studentList.value.forEach(e => {
+        //             studentNameList.value[e.pk] = e.fields.username
+        //         });
+        //     }
+        // }
+    } else {
+        user.value = undefined
+        // alert("请先登录，若无反应请刷新界面")
+        // router.push('/login')
+        router.push('/login_register')
+    }
+}
+is_login()
 </script>
 
 <template>
 <div></div>
-
+<Header :user="user"></Header>
 <div id='main'>
-    <div class="container" >
-        <div class="student">
-            <div class="get-student" @click="get_students_list">获取所有学生</div>
-            <div class="student-list">
-                <div class="student-item" v-for="student in studentList">
-                    <div class="information" id="account-id">学号: {{ student.accountId }}</div>
-                    <div class="information" id="username">姓名: {{ student.username }}</div>
-                    <div class="information" id="password">密码: {{ student.password }}</div>
-                    <div class="information" id="date-joined">创建时间: {{ student.date_joined }}</div>
-                    <div class="information" id="last-login">最后登录时间: {{ student.last_login }}</div>
-                </div>
-            </div>
+    
+    <SideBar :user="user" @selectItem="(key) => {selectedName = key}"></SideBar>
+    <div class="contain">
+        <div class="body">
+            <component :is="(pageList.find(e => e.name == selectedName)?.component) || 'div'"></component>
         </div>
-    </div>
-    <!-- <div class="container" >
-        <div class="patient">
-            <div class="get-patient" @click="get_all_patient">获取所有患者</div>
-            <div class="patient-list">
-                <div class="patient-item" v-for="patient in patientList">
-                    <div class="information" id="name">患者姓名: {{ patient.name }}</div>
-                    <div class="information" id="phone">患者电话: {{ patient.phone }}</div>
-                    <div class="information" id="student">所属诊所: {{ patient.student }}</div>
-                    <div class="information" id="birthday">患者生日: {{ patient.birthday }}</div>
-                    <div class="information" id="description">患者简介: {{ patient.description }}</div>
-                    <div class="information" id="register-time">患者创建时间: {{ patient.registerTime }}</div>
-                    <div class="information" id="mouth-image-number">患者口腔图片数量: {{ patient.mouthImageNumber }}</div>
-                </div>
-            </div>
-        </div>
-    </div> -->
+    </div>  
 </div>
-
+<Footer></Footer>
 </template>
 
 <style scoped>
 #main {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    /* 一定要先制定background在加其他限制 */
+    background: url("@img/home_background.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    justify-content: center;
+}
+.contain {
+    display: flex;
+    flex: 1;
+    justify-content: center;
+
+}
+.body {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.container {
+    height: 1300px;
+    top: 100px;
     width: 100%;
-    height: 1000px;
+    position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 100;
+
 }
-.container {
-    display: flex;
+.container_filter {
+    backdrop-filter: blur(1px);
+    -webkit-backdrop-filter: blur(1px);
+    background-color: rgba(0, 0, 0, 0.3);
 }
-.student, .patient {
-    margin: 20px;
-    /* height: 600px; */
-    width: 400px;
-    background-color: beige;
-    margin-left: 20px;
+
+
+/* 下面我们会解释这些 class 是做什么的 */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
 }
-.get-student, .get-patient {
-    cursor: pointer;
-}
-.student-item, .patient-item {
-    margin: 10px;
-    padding: 10px;
-    border: 1px solid black;
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
